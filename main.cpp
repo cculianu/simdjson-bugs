7058a94d567c9cdebfd2903eb7c0edfb2665a9e6
Calin Culianu <calin.culianu@gmail.com>
2020-10-24 13:46:42 +0300

    Added another wtf case

--- main.cpp
+++ main.cpp
@@ -56,6 +56,27 @@ void banner(const std::string_view &text)
     std::cout << std::string(80, '-') << "\n";
 }
 
+void wtf()
+{
+    banner("Weird results -- observe same string serialized for *ALL*\n"
+           ".. despite them all having come from different documents. Wtf?!");
+    std::vector<std::pair<std::string, simdjson::dom::element>> results;
+    simdjson::dom::parser parser; // it appears simdjson doesn't like this object to be re-used
+    for (const auto & f : files) {
+        auto buf = slurpFile(f);
+        std::cout << "Read \"" << f << "\" - size: " << buf.size() << "\n";
+        simdjson::dom::element parsed;
+        auto error = parser.parse(buf).get(parsed);
+        if (error) throw std::runtime_error(std::string{"Failed to parse "} + std::string{f});
+        results.emplace_back(std::string{f}, std::move(parsed));
+    }
+    for (const auto & [name, parsed] : results) {
+        std::cout << "File: \"" << name << "\"; serializing...\n";
+        std::cout << "First 100 bytes, reserialized: " << simdjson::to_string(parsed).substr(0, 100) << " ...\n";
+        std::cout << std::string(80, '-') << "\n";
+    }
+}
+
 void crashy()
 {
     banner("CRASHY!");
@@ -77,9 +98,32 @@ void crashy()
     }
 }
 
+void crashy2()
+{
+    banner("If the dom results outlive the parser -- also we crash! WHY?!");
+    std::list<std::pair<std::string, simdjson::dom::element>> results;
+    for (const auto & f : files) {
+        auto buf = slurpFile(f);
+        std::cout << "Read \"" << f << "\" - size: " << buf.size() << "\n";
+        results.emplace_back();
+        auto & [name, parsed] = results.back();
+        name = f; // assign
+        simdjson::dom::parser parser; // it appears simdjson doesn't like this object to be re-used
+        auto error = parser.parse(buf).get(parsed);
+        if (error) throw std::runtime_error(std::string{"Failed to parse "} + std::string{f});
+    }
+    // at this point the parser used for each result is dead.
+    for (const auto & [name, parsed] : results) {
+        std::cout << "File: \"" << name << "\"; serializing...\n";
+        std::cout << "First 100 bytes, reserialized: " << simdjson::to_string(parsed).substr(0, 100) << " ...\n";
+        std::cout << std::string(80, '-') << "\n";
+    }
+}
+
+
 void notcrashy()
 {
-    banner("NOT CRASHY!");
+    banner("NOT CRASHY! (works as one would expect)");
     // If we do this it works:
     std::list<std::tuple<std::string, simdjson::dom::parser, simdjson::dom::element>> results;
     for (const auto & f : files) {
@@ -101,9 +145,20 @@ void notcrashy()
 
 }
 
-int main() {   
-    notcrashy(); // this succeeds
-    crashy(); // this has a sigsegv on my Darwin macOS Mojave system with compiler: AppleClang 11.0.0.11000033
-              // also crashes on Linux G++ 8.3.0.  I actually couldn't find a system it *doesn't* crash on!
+int main() {
+    // this succeeds as expected
+    notcrashy();
+
+    // weird results -- why the same string printed for all?!?!
+    wtf();
+
+    // this has a sigsegv on my Darwin macOS Mojave system with compiler: AppleClang 11.0.0.11000033
+    // also crashes on Linux G++ 8.3.0.  I actually couldn't find a system it *doesn't* crash on!
+    crashy();
+
+    // (not reached unless you comment-out crashy() above)
+    // this also crashes.. if the DOM results outlive the parser. BOOM!
+    crashy2();
+
     return EXIT_SUCCESS;
 }
